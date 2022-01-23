9f2936a3bae5704b76c8b63ffb5b21685496c02c
toto <toto5032@gmail.com>
2022-01-23 19:09:20 +0900

    final

---
+++ CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by admin on 2022/01/23.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender:UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
